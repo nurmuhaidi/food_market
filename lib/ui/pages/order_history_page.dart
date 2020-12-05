ed6f3f26ecd9a5ffae5224af680aeedeba83129d
nurmuhaidi <171240000673@unisnu.ac.id>
2020-12-05 22:00:29 +0700

    FM20 - Order History Page Part 2

--- lib/ui/pages/order_history_page.dart
+++ lib/ui/pages/order_history_page.dart
@@ -30,6 +30,8 @@ class _OrderHistoryPageState extends State<OrderHistoryPage> {
         buttonTap1: () {},
       );
     } else {
+      double listItemWidth =
+          MediaQuery.of(context).size.width - 2 * defaultMargin;
       return ListView(
         children: [
           Column(
@@ -78,7 +80,17 @@ class _OrderHistoryPageState extends State<OrderHistoryPage> {
                     ),
                     Column(
                       children: (selectedIndex == 0 ? inProgress : past)
-                          .map((e) => Text(e.food.name))
+                          .map(
+                            (e) => Padding(
+                              padding: const EdgeInsets.only(
+                                right: defaultMargin,
+                                left: defaultMargin,
+                                bottom: 16,
+                              ),
+                              child: OrderListItem(
+                                  transaction: e, itemWidth: listItemWidth),
+                            ),
+                          )
                           .toList(),
                     )
                   ],
